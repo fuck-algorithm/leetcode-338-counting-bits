7717aea0caf11b1eb4aa71b9e65db19d47f8a23d
CC11001100 <CC11001100@qq.com>
2025-04-18 14:52:22 +0800

    fix: 修改为Hash路由方式，适配GitHub Pages部署

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { createHashRouter } from 'react-router-dom';
+import App from './App';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+export default router; 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fuck-algorithm/leetcode-338-counting-bits/', // GitHub Pages子路径
+  base: '/leetcode-338-counting-bits/', // GitHub Pages子路径
   build: {
     outDir: 'dist', // 输出目录
     assetsDir: 'assets', // 静态资源目录
